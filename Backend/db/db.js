9577b663c93c8ea122193aeb2bdf9d53fecf5579
Vishal Kandu <kanduvishal07@gmail.com>
2025-06-14 22:07:49 +0530

    feat: Implement captain registration and login functionality

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose');
 
-function connectToDb() {
-    // mongoose.connect(process.env.DB_CONNECT, {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    // }, () => {
-    //     console.log("Connected to MongoDB");
-    // }).catch(err => {
-    //     console.log("Error connecting to MongoDB:", err);
-    //     process.exit(1); // Exit the process if connection fails
-    // })
 
-    mongoose.connect(process.env.DB_CONNECT).then(() => {
-        console.log("Connected to MongoDB");
-    }).catch(err => {
-        console.log("Error connecting to MongoDB:", err);
-        process.exit(1); // Exit the process if connection fails
-    });
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+        console.log('Connected to DB');
+    }).catch(err => console.log(err));
 }
+
 
 module.exports = connectToDb;
